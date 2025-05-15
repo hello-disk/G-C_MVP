8a047fdc6331e040842ff1100e28c28468e67843
hello-disk <buddykoala@csu.fullerton.edu>
2025-05-14 21:31:45 -0700

    Initial Commit

---
+++ G@C_MVP/G_C_MVPApp.swift
@@ -0,0 +1,32 @@
+//
+//  G_C_MVPApp.swift
+//  G@C_MVP
+//
+//  Created by HARRISON TONG on 5/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct G_C_MVPApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ G@C_MVP/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  G@C_MVP
+//
+//  Created by HARRISON TONG on 5/14/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
